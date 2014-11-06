4574051fdfb83c27a55acc93b3a0a3be259984ad
sheynandr <sheynandr@gmail.com>
2014-11-06 07:11:23 +0600

    EDit!!!

--- sshpwd.sh
+++ sshpwd.sh
@@ -4,13 +4,13 @@ echo "Введите enable для включения или disable для вы
 echo "Также можно использовать команду enable gen для генерации временного пароля."
 echo "Что нужно? " && read status
 if [ "$status" = "enable" ]; then
-sed -i 's/PubkeyAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 service ssh restart
 elif [ "$status" = "disable" ]; then
-sed -i 's/PubkeyAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
 service ssh restart
 elif [ "$status" = "enable gen" ]; then
-sed -i 's/PubkeyAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 service ssh restart
 echo "Пароль для пользователя root" $password
 echo root:$password | chpasswd
